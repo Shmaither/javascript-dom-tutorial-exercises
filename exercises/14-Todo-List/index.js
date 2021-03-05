17c1e19efc551e1f3d3ae4063d09c45b25986c17
Shmaither M. Rizo <shmaither@gmail.com>
2021-03-05 03:09:08 +0000

    Up to exercise 14 unfinished

--- exercises/14-Todo-List/index.js
+++ exercises/14-Todo-List/index.js
@@ -1,1 +1,11 @@
-// Your code here
+let container = document.querySelector("#container");
+
+window.onload = function() {
+	console.log("The website was loaded...");
+	container.innerHTML += `<button>Add Task</button>` + `<button>Delete Task</button>`;
+
+	//let select = document.querySelector("#mySelect");
+	//countries.forEach(elem => {
+	//	select.innerHTML += `<option value="${elem}">${elem}</option>`;
+	//});
+};
