7c2fba5b79e8413b001759af791252b7bc5cdb76
Shmaither M. Rizo <shmaither@gmail.com>
2021-03-05 01:18:33 +0000

    Up to exercise 05

--- .breathecode/resets/05-Create-DOM-Element-First/index.js
+++ .breathecode/resets/05-Create-DOM-Element-First/index.js
@@ -4,8 +4,3 @@ document.querySelector("#myDiv").appendChild(elem);
 
 //Your code here
 // Don'change the code above
-
-var elemP = document.createElement("p");
-elemP.innerHTML = "Hello World";
-elemP.style.background = "yellow";
-document.querySelector("#myDiv").appendChild(elemP);
