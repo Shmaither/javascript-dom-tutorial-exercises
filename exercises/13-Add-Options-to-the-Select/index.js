aa3600c01dc33b20c99d2ab2c1370b8bb77bad5d
Shmaither M. Rizo <shmaither@gmail.com>
2021-03-05 02:49:50 +0000

    Up to exercise 13

--- exercises/13-Add-Options-to-the-Select/index.js
+++ exercises/13-Add-Options-to-the-Select/index.js
@@ -2,5 +2,14 @@ window.onload = function() {
 	console.log("The website was loaded...");
 	let countries = ["USA", "France", "Italy", "Brazil", "Colombia", "Belize", "Venezuela"];
 
-  // your code here
+	let select = document.querySelector("#mySelect");
+	countries.forEach(elem => {
+		select.innerHTML += `<option value="${elem}">${elem}</option>`;
+	});
 };
+
+let button = document.querySelector("#superbutton");
+button.addEventListener("click", function() {
+	let selectRetrieve = document.querySelector("#mySelect").value;
+	alert(selectRetrieve);
+});
