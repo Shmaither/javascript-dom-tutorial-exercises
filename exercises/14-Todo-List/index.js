a9ac2f5c21eaaf19d22329c3fb8db1b9b6c2b48f
Shmaither M. Rizo <shmaither@gmail.com>
2021-03-06 03:45:33 +0000

    Up to exercise 14

--- exercises/14-Todo-List/index.js
+++ exercises/14-Todo-List/index.js
@@ -1,11 +1,38 @@
-let container = document.querySelector("#container");
-
 window.onload = function() {
 	console.log("The website was loaded...");
-	container.innerHTML += `<button>Add Task</button>` + `<button>Delete Task</button>`;
 
-	//let select = document.querySelector("#mySelect");
-	//countries.forEach(elem => {
-	//	select.innerHTML += `<option value="${elem}">${elem}</option>`;
-	//});
+	let button = document.createElement("button");
+	button.id = "myButton";
+	button.innerHTML = "Add Task";
+	document.querySelector("#container").appendChild(button);
+
+	let myButton = document.querySelector("#myButton");
+	myButton.addEventListener("click", function() {
+		let newTask = document.querySelector("#addToDo").value;
+		console.log(newTask);
+
+		let father = document.querySelector("ul");
+		let child = document.createElement("li");
+		child.innerHTML = '<span><i class="fa fa-trash"></i></span>' + newTask;
+		father.appendChild(child);
+
+		call();
+	});
 };
+
+function call() {
+	const lis = document.querySelectorAll("li");
+	console.log(lis);
+
+	// for (const li of lis) {
+	// 	li.addEventListener("click", function() {
+	// 		this.parentNode.removeChild(this);
+	// 	});
+	// }
+
+	lis.forEach(elem => {
+		elem.addEventListener("click", function() {
+			elem.parentNode.removeChild(elem);
+		});
+	});
+}
